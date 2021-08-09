001a707f0535dc4f5d6a5ebf1f590a583d83d8e2
saeedsugdan <86883420+saeedsugdan@users.noreply.github.com>
2021-08-09 08:57:30 -0700

    saeed

---
+++ assment1.php
@@ -0,0 +1,70 @@
+<?php 
+
+// 1
+// $hexMessage = '596f7520636f6e76657274656420697420636f72726563746c7921';
+
+// echo hex2bin($hexMessage);
+
+// 2
+// $htmlContent = "<h1>PHP track</h1><p>PHP is an interpreted language</p>";
+// $htmlContent = "this is post 3aady <script>alert('hello')</script>";
+
+// // echo $htmlContent;
+// // echo strip_tags($htmlContent);
+// echo htmlspecialchars($htmlContent);
+
+// 3
+// $username = "  Kareem Fouad ";
+// $username = trim($username);
+// var_dump($username);
+
+// 4
+
+
+//**************
+// $num = 15023198.91;
+
+// echo number_format($num, 2);
+
+// 5
+
+//**************
+
+
+// $employeePositions = [
+//     'kareem fouad' => 'backend',
+//     'ahmed bahnasy' => 'frontend',
+//     'mohammed nabeel' => 'android',
+// ];
+
+// $employees = array_keys($employeePositions);
+// $positions = array_values($employeePositions);
+
+// echo '<pre>';
+// print_r($employees);
+// print_r($positions);
+// echo '</pre>';
+
+// 6
+
+
+//**************
+// $nums = [4, 7, 1];
+
+// // list($x, $y, $z) = $nums;
+// [$x, $y, $z] = $nums;
+
+// echo $x, $y, $z;
+
+// 7
+
+//**************
+// $userData = [
+//     'name' => 'kareem',
+//     'job' => 'backend',
+//     'language' => 'php',
+//     'framework' => 'laravel',
+// ];
+
+// extract($userData);
+// echo $name, $job, $language, $framework;
